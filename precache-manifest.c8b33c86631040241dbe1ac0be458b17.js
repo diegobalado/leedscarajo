7b3402c4814c4114031520bb6e17ff754eab55b0
Diego <jdiegomdq@gmail.com>
2020-06-28 04:01:52 -0300

    Updates

---
+++ precache-manifest.c8b33c86631040241dbe1ac0be458b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3031c847a82d6fa3cd7b26ec800c523",
+    "url": "/leedscarajo/index.html"
+  },
+  {
+    "revision": "66f96f6e2be8e41530dd",
+    "url": "/leedscarajo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17b03d71495c06b43556",
+    "url": "/leedscarajo/static/js/2.fbce9e50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/leedscarajo/static/js/2.fbce9e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f96f6e2be8e41530dd",
+    "url": "/leedscarajo/static/js/main.9fd4f4eb.chunk.js"
+  },
+  {
+    "revision": "a188dc7ac5680cf54a7d",
+    "url": "/leedscarajo/static/js/runtime-main.5611f7ec.js"
+  }
+]);
