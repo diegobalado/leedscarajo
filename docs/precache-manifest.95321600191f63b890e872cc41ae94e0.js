706799bb9b28d18f8a70148971bc4e1155c4243e
Diego <jdiegomdq@gmail.com>
2020-06-28 01:51:26 -0300

    adds build to docs script

---
+++ docs/precache-manifest.95321600191f63b890e872cc41ae94e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb03eb574edd2e14a5f946a7bb4aa36a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0cf9106a2a58acb5752",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd9ca7e1296466f03f6e",
+    "url": "/static/js/2.f13ccc40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f13ccc40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0cf9106a2a58acb5752",
+    "url": "/static/js/main.8c257bda.chunk.js"
+  },
+  {
+    "revision": "236525a621ff2846d318",
+    "url": "/static/js/runtime-main.47c9c64c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
